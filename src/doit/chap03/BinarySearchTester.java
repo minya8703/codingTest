f9eba4e51e755ad3c4b6163379a1ce4d2f383435
MINYA <minya8703@gmail.com>
2022-09-16 02:34:06 +0900

    Do It chap03

---
+++ src/doit/chap03/BinarySearchTester.java
@@ -0,0 +1,15 @@
+package doit.chap03;
+
+import java.util.Scanner;
+
+public class BinarySearchTester {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("요솟수 : ");
+        int num = sc.nextInt();
+        int[] x = new int[num];
+
+
+    }
+}
